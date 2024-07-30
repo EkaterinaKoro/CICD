371f551b141ae919768e4d2402adbf2d6ec21d94
Kate <cimonyu@mail.ru>
2024-07-30 19:30:38 +0300

    сборка.md

---
+++ src/test/java/MaxServiceTest.java
@@ -0,0 +1,19 @@
+package test.java;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MaxServiceTest {
+
+    @Test
+    public void ShouldFinMax() {
+        MaxService maxService = new MaxService();
+        int a = 5;
+        int b = 3;
+
+        int expected = a;
+        int actual = maxService.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
